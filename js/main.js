e251237b7821bfd9f67b9764efc7aed3a6fd3405
Aya-Saleh-Mahgoub <ayasalehmahgoub23@gmail.com>
2024-06-10 20:52:56 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var siteName = document.getElementById("bookmarkName");
 var siteURL = document.getElementById("bookmarkURL");
 var submitBtn = document.getElementById("submitBtn");
 var tableContent = document.getElementById("tableContent");
+var trueNameRegex = document.querySelector('.icons-name-regex i.fa-solid.fa-check');
+var falseNameRegex = document.querySelector('.icons-name-regex i.fa-solid.fa-xmark');
+var trueUrlRegex = document.querySelector('.icons-url-regex i.fa-solid.fa-check');
+var falseUrlRegex = document.querySelector('.icons-url-regex i.fa-solid.fa-xmark');
 var bookmarks=[];
 var mainIndex=0;
 if(localStorage.getItem("bookmarks")!==null){
@@ -11,15 +15,12 @@ if(localStorage.getItem("bookmarks")!==null){
 var nameRegex = /^[A-Za-z_]{1,}$/;
 function nameValidate(){
     if(nameRegex.test(siteName.value)){
-        
         return true;
     }else{
-        
         return false;
     }
 }
 var urlRegex = /^(https|http):\/\/(www\.)[A-Za-z0-9_\.]{1,}\.[a-z]{3}$/;
-
 function urlValidate(){
     if(urlRegex.test(siteURL.value)){
         
@@ -30,25 +31,34 @@ function urlValidate(){
     }
 }
 siteName.onkeyup = function(){
-    if(nameValidate() && urlValidate()){
+    if(nameValidate()){
         siteName.style.borderColor = "green";
+        trueNameRegex.style.display = "block";
+        falseNameRegex.style.display = "none";
         submitBtn.removeAttribute("disabled");
     }else{
         siteName.style.borderColor = "red";
+        trueNameRegex.style.display = "none";
+        falseNameRegex.style.display = "block";
         submitBtn.disabled="true";
     }
 }
 siteURL.onkeyup = function(){
-    if(nameValidate() && urlValidate()){
+    if(urlValidate()){
         siteURL.style.borderColor = "green";
+        trueUrlRegex.style.display = "block";
+        falseUrlRegex.style.display = "none";
         submitBtn.removeAttribute("disabled");
     }else{
         siteURL.style.borderColor = "red";
+        trueUrlRegex.style.display = "none";
+        falseUrlRegex.style.display = "block";
         submitBtn.disabled="true";
     }
 }
-console.log('testURL',urlRegex.test('wwww.google.com'));
-console.log(nameRegex.test("0mM00"));
+console.log('test http://www',urlRegex.test('www.google.com'));
+console.log('test https://www',urlRegex.test('www.google.com'));
+console.log(nameRegex.test("route"));
 
 submitBtn.onclick = function(){
     if(submitBtn.innerHTML == "Update"){
@@ -89,8 +99,8 @@ function displayBookmarks(arr){
                        </a
                      </td>
                      <td>
-                        <button onclick="updateBook(${i})" class="btn btn-info pe-2">
-                         <i class=""></i>
+                        <button onclick="updateBook(${i})" class="btn btn-success  pe-2">
+                         <i class="fas fa-edit"></i>
                          Update
                        </button>
                       </td>
@@ -129,179 +139,3 @@ function search(term){
         displayBookmarks(searchedBook);
     }
 }
-// var deleteBtns;
-// var visitBtns;
-// var closeBtn = document.getElementById("closeBtn");
-// var boxModal = document.querySelector(".box-info");
-// var bookmarks = [];
-
-// if (localStorage.getItem("bookmarksList")) {
-//   bookmarks = JSON.parse(localStorage.getItem("bookmarksList"));
-//   for (var x = 0; x < bookmarks.length; x++) {
-//     displayBookmark(x);
-//   }
-// }
-
-// // =====> Display Function and adding click event to visit and delete buttons
-
-// function displayBookmark(indexOfWebsite) {
-//   var userURL = bookmarks[indexOfWebsite].siteURL;
-//   var httpsRegex = /^https?:\/\//g;
-//   if (httpsRegex.test(userURL)) {
-//     validURL = userURL;
-//     fixedURL = validURL
-//       .split("")
-//       .splice(validURL.match(httpsRegex)[0].length)
-//       .join("");
-//   } else {
-//     var fixedURL = userURL;
-//     validURL = `https://${userURL}`;
-//   }
-//   var newBookmark = `
-//               <tr>
-//                 <td>${indexOfWebsite + 1}</td>
-//                 <td>${bookmarks[indexOfWebsite].siteName}</td>              
-//                 <td>
-//                   <button class="btn btn-visit" data-index="${indexOfWebsite}">
-//                     <i class="fa-solid fa-eye pe-2"></i>Visit
-//                   </button>
-//                 </td>
-//                 <td>
-//                   <button class="btn btn-delete pe-2" data-index="${indexOfWebsite}">
-//                     <i class="fa-solid fa-trash-can"></i>
-//                     Delete
-//                   </button>
-//                 </td>
-//             </tr>
-//             `;
-//   tableContent.innerHTML += newBookmark;
-
-//   // =====> Adding Click Event to All delete buttons every time a new bookmark being added
-
-//   deleteBtns = document.querySelectorAll(".btn-delete");
-//   if (deleteBtns) {
-//     for (var j = 0; j < deleteBtns.length; j++) {
-//       deleteBtns[j].addEventListener("click", function (e) {
-//         deleteBookmark(e);
-//       });
-//     }
-//   }
-
-//   // =====> Adding Click Event to All visit buttons every time a new bookmark being added
-
-//   visitBtns = document.querySelectorAll(".btn-visit");
-//   if (visitBtns) {
-//     for (var l = 0; l < visitBtns.length; l++) {
-//       visitBtns[l].addEventListener("click", function (e) {
-//         visitWebsite(e);
-//       });
-//     }
-//   }
-// }
-
-// // =====> Clear Input Function
-
-// function clearInput() {
-//   siteName.value = "";
-//   siteURL.value = "";
-// }
-
-// // =====> Capitalize Function ==> take string and makes it capitalize
-
-// function capitalize(str) {
-//   let strArr = str.split("");
-//   strArr[0] = strArr[0].toUpperCase();
-//   return strArr.join("");
-// }
-
-// // =====> Submit Function
-
-// submitBtn.addEventListener("click", function () {
-//   if (
-//     siteName.classList.contains("is-valid") &&
-//     siteURL.classList.contains("is-valid")
-//   ) {
-//     var bookmark = {
-//       siteName: capitalize(siteName.value),
-//       siteURL: siteURL.value,
-//     };
-//     bookmarks.push(bookmark);
-//     localStorage.setItem("bookmarksList", JSON.stringify(bookmarks));
-//     displayBookmark(bookmarks.length - 1);
-//     clearInput();
-//     siteName.classList.remove("is-valid");
-//     siteURL.classList.remove("is-valid");
-//   } else {
-//     boxModal.classList.remove("d-none");
-//   }
-// });
-
-// // =====> Delete Function
-
-// function deleteBookmark(e) {
-//   tableContent.innerHTML = "";
-//   var deletedIndex = e.target.dataset.index;
-//   bookmarks.splice(deletedIndex, 1);
-//   for (var k = 0; k < bookmarks.length; k++) {
-//     displayBookmark(k);
-//   }
-//   localStorage.setItem("bookmarksList", JSON.stringify(bookmarks));
-// }
-
-// // =====> Visit Function
-
-// function visitWebsite(e) {
-//   var websiteIndex = e.target.dataset.index;
-//   var httpsRegex = /^https?:\/\//;
-//   if (httpsRegex.test(bookmarks[websiteIndex].siteURL)) {
-//     open(bookmarks[websiteIndex].siteURL);
-//   } else {
-//     open(`https://${bookmarks[websiteIndex].siteURL}`);
-//   }
-// }
-
-// // =====> Making sure that user enter the correct data
-
-// var nameRegex = /^\w{3,}(\s+\w+)*$/;
-// var urlRegex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
-
-// siteName.addEventListener("input", function () {
-//   validate(siteName, nameRegex);
-// });
-
-// siteURL.addEventListener("input", function () {
-//   validate(siteURL, urlRegex);
-// });
-
-// function validate(element, regex) {
-//   var testRegex = regex;
-//   if (testRegex.test(element.value)) {
-//     element.classList.add("is-valid");
-//     element.classList.remove("is-invalid");
-//   } else {
-//     element.classList.add("is-invalid");
-//     element.classList.remove("is-valid");
-//   }
-// }
-
-// //Close Modal Function
-
-// function closeModal() {
-//   boxModal.classList.add("d-none");
-// }
-
-// // 3 ways to close modal => close button -  Esc key - clicking outside modal
-
-// closeBtn.addEventListener("click", closeModal);
-
-// document.addEventListener("keydown", function (e) {
-//   if (e.key == "Escape") {
-//     closeModal();
-//   }
-// });
-
-// document.addEventListener("click", function (e) {
-//   if (e.target.classList.contains("box-info")) {
-//     closeModal();
-//   }
-// });
